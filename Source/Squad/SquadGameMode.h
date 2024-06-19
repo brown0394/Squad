979ce2ca5b5a31d9c9a2e3861a3c89e9463d0217
SUNG CHAN CHOI <73463402+brown0394@users.noreply.github.com>
2024-06-19 13:34:59 +0900

    초기 커밋.

---
+++ Source/Squad/SquadGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SquadGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ASquadGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ASquadGameMode();
+};
+
+
+
