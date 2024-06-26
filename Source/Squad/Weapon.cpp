aa39f40c838ae55b8abecdee6a8d65c7f4afd483
SUNG CHAN CHOI <73463402+brown0394@users.noreply.github.com>
2024-06-26 15:27:44 +0900

    create base class for weapons

--- Source/Squad/Weapon.cpp
+++ Source/Squad/Weapon.cpp
@@ -6,28 +6,5 @@
 // Sets default values
 AWeapon::AWeapon()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
-}
 
-AWeapon::AWeapon(FName& name) : weaponType(name) {
-	PrimaryActorTick.bCanEverTick = true;
-}
-
-// Called when the game starts or when spawned
-void AWeapon::BeginPlay()
-{
-	Super::BeginPlay();
-	
-}
-
-// Called every frame
-void AWeapon::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
-
-}
-
-FName AWeapon::GetWeaponType() {
-	return weaponType;
 }
--- Source/Squad/Weapon.h
+++ Source/Squad/Weapon.h
@@ -14,15 +14,6 @@ class SQUAD_API AWeapon : public AActor
 public:	
 	// Sets default values for this actor's properties
 	AWeapon();
-	AWeapon(FName& name);
 	virtual void DoAttack() PURE_VIRTUAL(AWeapon::DoAttack, ;);
-	FName GetWeaponType();
-protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
-	FName weaponType;
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
 
 };
